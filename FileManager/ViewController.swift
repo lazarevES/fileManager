65c2426a38ed2b30a45c22e8577116bfd1f88320
Lazarev Egor <lazarev_es@sima-land.ru>
2022-07-05 19:39:21 +0500

    init commit

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by Егор Лазарев on 05.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
